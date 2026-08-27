1868c9f8c469c8decb9b4757400c89b37d0f645f
ST10479225 <st10479225@imconnect.edu.za>
2026-08-27 19:46:48 +0000

    pom file

---
+++ src/main/java/com/mycompany/progicetask1/PROGicetask1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progicetask1;
+
+/**
+ *
+ * @author emeris
+ */
+public class PROGicetask1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
